3bce5f2f4f8f8583ebacf0b2cca279ef5bcfa3c8
YASH FADADU <fadaduyash1994@gmail.com>
2025-05-01 16:30:43 +0530

    Profile error solved

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,11 @@
-import { auth, signIn, signOut } from "@/auth";
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
+import { auth, signOut, signIn } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = async () => {
-  const sessionn = await auth();
+  const session = await auth();
 
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
@@ -13,27 +15,44 @@ const Navbar = async () => {
         </Link>
 
         <div className="flex items-center gap-5 text-black">
-          {sessionn && sessionn?.user ? (
+          {session && session?.user ? (
             <>
               <Link href="/startup/create">
-                <span>Create</span>
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden" />
               </Link>
 
-              <form action={async () => {
-                "use server";
-                await signOut({redirectTo: "/"})}}>
-                <button type="submit">Logout</button>       
+              <form
+                action={async () => {
+                  "use server";
 
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout</span>
+                  <LogOut className="size-6 sm:hidden text-red-500" />
+                </button>
               </form>
 
-              <Link href={`/user/${sessionn?.user?.id}`}>
-                <span>{sessionn?.user?.name}</span>
+              <Link href={`/user/${session?.id}`}>
+                <Avatar className="size-10">
+                  <AvatarImage
+                    src={session?.user?.image || ""}
+                    alt={session?.user?.name || ""}
+                  />
+                  <AvatarFallback>AV</AvatarFallback>
+                </Avatar>
               </Link>
             </>
           ) : (
-            <form action={async () => {
+            <form
+              action={async () => {
                 "use server";
-                await signIn("github")}}>
+
+                await signIn("github");
+              }}
+            >
               <button type="submit">Login</button>
             </form>
           )}
